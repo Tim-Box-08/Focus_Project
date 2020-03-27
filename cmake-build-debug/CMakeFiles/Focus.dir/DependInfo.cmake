3a16c6a8e7f5b85e3e7510fa1110f6021f0c2d18
Tim_Box_08 <ttotoshea@gmail.com>
2020-03-27 11:56:22 +0000

    First commit with project initalisation

---
+++ cmake-build-debug/CMakeFiles/Focus.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "C:/Users/ttoto/CLionProjects/Focus/game_init.c" "C:/Users/ttoto/CLionProjects/Focus/cmake-build-debug/CMakeFiles/Focus.dir/game_init.c.obj"
+  "C:/Users/ttoto/CLionProjects/Focus/input_output.c" "C:/Users/ttoto/CLionProjects/Focus/cmake-build-debug/CMakeFiles/Focus.dir/input_output.c.obj"
+  "C:/Users/ttoto/CLionProjects/Focus/main.c" "C:/Users/ttoto/CLionProjects/Focus/cmake-build-debug/CMakeFiles/Focus.dir/main.c.obj"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
