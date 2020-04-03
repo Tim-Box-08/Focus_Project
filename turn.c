7016d34df32052d59363a4eda35eefbb19578e50
Tim_Box_08 <ttotoshea@gmail.com>
2020-04-03 15:59:36 +0100

    Implemented function to check if a players move is valid

--- turn.c
+++ turn.c
@@ -4,6 +4,9 @@
 #include "game_init.c"
 #include "turn.h"
 #include <stdio.h>
+#include <stdbool.h>
+
+bool moveCheck(int moveType, int curRow, int curCol)
 
 void playerTurn(player currentPlayer, square board[BOARD_SIZE][BOARD_SIZE])
 {
@@ -21,7 +24,7 @@ void playerTurn(player currentPlayer, square board[BOARD_SIZE][BOARD_SIZE])
             h = 1;
         }
         else{
-            printf("Please select a valid square.\n Valid squares have the same colour as the current player and are valid squares on the board");
+            printf("Please select a valid square.\n Valid squares have the same colour as the current player and are valid positions on the board");
         }
     }
 
@@ -32,28 +35,175 @@ void playerTurn(player currentPlayer, square board[BOARD_SIZE][BOARD_SIZE])
     {
         printf("Please select a direction in which to move the piece\nEnter 1 for left, 2 for right, 3 for up and 4 for down:");
         scanf("%d", &moveType);
-
-        switch (moveType)
+        if(moveCheck(moveType, curRow, curCol) == true) {
+            switch (moveType) {
+                case 1:
+                    curRow = curRow;
+                    curCol -= curCol;
+                    break;
+                case 2:
+                    curRow = curRow;
+                    curCol += curCol;
+                    break;
+                case 3:
+                    curRow -= curRow;
+                    curCol = curCol;
+                    break;
+                case 4:
+                    curRow += curRow;
+                    curCol = curCol;
+            }
+            continue;
+        }
+        else
         {
-            case 1:
-                curRow = curRow;
-                curCol -= curCol;
-                break;
-            case 2:
-                curRow = curRow;
-                curCol += curCol;
-                break;
-            case 3:
-                curRow -= curRow;
-                curCol = curCol;
-                break;
-            case 4:
-                curRow += curRow;
-                curCol = curCol;
+            printf("Selected move is invalid\n");
+            moveNum -= 1;
+            continue;
         }
     }
-
-
-
 }
 
+bool moveCheck(int moveType, int curRow, int curCol)
+{
+   bool validMove = true;
+
+   //Switch statement will check the proposed move against a list of invalid moves based on the players current position on the board
+   switch(curRow)
+   {
+       case 0:
+           switch (curCol) {
+               case 2:
+                   if (moveType == 3 || moveType == 1) {
+                       validMove = false;
+                   }
+               case 3:
+                   if (moveType == 3) {
+                       validMove = false;
+                   }
+                   break;
+               case 4:
+                   if (moveType == 3) {
+                       validMove = false;
+                   }
+                   break;
+               case 5:
+                   if (moveType == 3 || moveType == 2) {
+                       validMove = false;
+                   }
+                   break;
+           }
+           break;
+       case 1:
+           switch (curCol) {
+               case 1:
+                   if (moveType == 3 || moveType == 1) {
+                       validMove = false;
+                   }
+                   break;
+               case 6:
+                   if (moveType == 3 || moveType == 2) {
+                       validMove = false;
+                   }
+                   break;
+           }
+           break;
+       case 2:
+           switch (curCol) {
+               case 0:
+                   if (moveType == 3 || moveType == 1) {
+                       validMove = false;
+                   }
+                   break;
+               case 7:
+                   if (moveType == 3 || moveType == 2) {
+                       validMove = false;
+                   }
+           }
+           break;
+       case 3:
+           switch (curCol) {
+               case 0:
+                   if (moveType == 1) {
+                       validMove = false;
+                   }
+                   break;
+               case 7:
+                   if (moveType == 2) {
+                       validMove = false;
+                   }
+                   break;
+           }
+           break;
+       case 4:
+           switch (curCol) {
+               case 0:
+                   if (moveType == 1) {
+                       validMove = false;
+                   }
+                   break;
+               case 7:
+                   if (moveType == 2) {
+                       validMove = false;
+                   }
+                   break;
+           }
+           break;
+       case 5:
+           switch (curCol) {
+               case 0:
+                   if (moveType == 1 || moveType == 4) {
+                       validMove = false;
+                   }
+                   break;
+               case 7:
+                   if (moveType == 2 || moveType == 4) {
+                       validMove = false;
+                   }
+                   break;
+           }
+           break;
+       case 6:
+           switch (curCol) {
+
+               case 1:
+                   if (moveType == 1 || moveType == 4) {
+                       validMove = false;
+                   }
+                   break;
+               case 6:
+                   if (moveType == 2 || moveType == 4) {
+                       validMove = false;
+                   }
+                   break;
+           }
+           break;
+       case 7:
+           switch(curCol)
+           {
+               case 2:
+                   if (moveType == 1 || moveType == 4) {
+                       validMove = false;
+                   }
+               break;
+               case 3:
+                   if (moveType == 4) {
+                       validMove = false;
+                   }
+               break;
+               case 4:
+                   if (moveType == 4) {
+                       validMove = false;
+                   }
+               break;
+               case 5:
+                   if (moveType == 2 || moveType == 4) {
+                       validMove = false;
+                   }
+               break;
+           }
+           break;
+   }
+
+   return validMove;
+}
