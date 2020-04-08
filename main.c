6d3a877e5a9575db8c02917bbe1150dcf30d9046
Tim_Box_08 <ttotoshea@gmail.com>
2020-04-08 14:23:39 +0100

    Added commenting to code where nessisary

--- main.c
+++ main.c
@@ -8,11 +8,11 @@ int main()
     player players[PLAYERS_NUM];
     square board[BOARD_SIZE][BOARD_SIZE];
 
-    initialize_players(players);
+    initialize_players(players);//Call to initialize_players function which is contained within game_init.h
 
-    initialize_board(board);
+    initialize_board(board);//Call to initialize_board function which is contained within game_init.h
 
-    print_board(board);
+    print_board(board);//Call to print_board function which will print the initialised board
 
     for(int t=0;t<5;t++)
     {
--- turn.c
+++ turn.c
@@ -6,13 +6,16 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+//Function prototypes
 bool moveCheck(int moveType, int curRow, int curCol);
 void merge(square board[BOARD_SIZE][BOARD_SIZE],int k, int t, int curRow, int curCol);
 
-void playerTurn(player currentPlayer, square board[BOARD_SIZE][BOARD_SIZE]) {
+void playerTurn(player currentPlayer, square board[BOARD_SIZE][BOARD_SIZE])
+{
     int k = 0, t = 0, h = 0, moveNum, moveType, curRow, curCol;
 
     //Allowing the player to select a piece
+    //Statement will loop until a valid piece is selected
     while (h == 0) {
         printf("Please select a row:");
         scanf("%d", &k);
@@ -26,15 +29,18 @@ void playerTurn(player currentPlayer, square board[BOARD_SIZE][BOARD_SIZE]) {
         }
     }
 
+    //curRow and curCol will act as a cursor used to track position on the board
     curRow = k;
     curCol = t;
 
+    //Number of square a piece will move is decide by size of stack
     for (moveNum = 0; moveNum < board[k][t].num_pieces; moveNum++)
     {
         printf("You are currently at position [%d][%d]\n", curRow, curCol);
         printf("Please select a direction in which to move the piece\nEnter 1 for left, 2 for right, 3 for up and 4 for down:");
         scanf("%d", &moveType);
 
+        //Cursor will move around the board based on the direction a player wishes to move
         if (moveCheck(moveType, curRow, curCol) == true) {
             switch (moveType) {
                 case 1:
@@ -54,37 +60,42 @@ void playerTurn(player currentPlayer, square board[BOARD_SIZE][BOARD_SIZE]) {
                     curCol = curCol;
             }
             continue;
-        } else {
+        }
+        //Statement will trigger if selected move is not a valid one based on the cursors current position
+        else {
             printf("Selected move is invalid\n");
             moveNum -= 1;
             continue;
         }
     }
 
-    printf("Calling push function\n");
-    merge(board, k, t, curRow, curCol);
-    board[k][t].stack = NULL;
-    board[k][t].num_pieces = 0;
+    merge(board, k, t, curRow, curCol);//Call to merge function which will combine the two identified stacks
+    board[k][t].stack = NULL;//Resets the pointer in the now vacant board square to NULL
+    board[k][t].num_pieces = 0;//Resets the conter for the number of pieces on the now vacant board square to 0
 }
 
+//Function to merge two stacks together
 void merge(square board[BOARD_SIZE][BOARD_SIZE], int k, int t, int curRow, int curCol)
 {
-    piece * curr;
-    curr = board[k][t].stack;
+    piece * curr;//Pointer to a struct of type piece
+    curr = board[k][t].stack;//Curr is initialised with the address of the top of the stack to be added
+    //While loop will continue execution until the bottom of the stack is reached
     while(curr->next !=NULL)
     {
         curr = curr->next;
     }
-    curr->next = board[curRow][curCol].stack;
-    board[curRow][curCol].stack = board[k][t].stack;
-    board[curRow][curCol].num_pieces += board[k][t].num_pieces;
+    curr->next = board[curRow][curCol].stack;//The pointer at the bottom of the stack is updated to point to the top of the other stack
+    board[curRow][curCol].stack = board[k][t].stack;//Makes it so that the board pointer now points to the new top of the stack
+    board[curRow][curCol].num_pieces += board[k][t].num_pieces;//Updates the counter for the number of pieces in the stack
 }
 
+//Function to check the validity of a player's move
 bool moveCheck(int moveType, int curRow, int curCol)
 {
-   bool validMove = true;
+   bool validMove = true;//Move is initial assumed to be valid
 
    //Switch statement will check the proposed move against a list of invalid moves based on the players current position on the board
+   //If a proposed move is invalid the statement will alter the validMove bool to reflect that fact
    switch(curRow)
    {
        case 0:
@@ -221,5 +232,5 @@ bool moveCheck(int moveType, int curRow, int curCol)
            break;
    }
 
-    return validMove;
+    return validMove;//Returns a boolean which reflects the validity of the move
 }
