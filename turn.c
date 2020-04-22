2681f26cc291b3ccf0d4ff011136e38a72153f3f
Tim_Box_08 <ttotoshea@gmail.com>
2020-04-22 12:48:57 +0100

    Finished implementation of functionality to split stacks to allow for moment

--- turn.c
+++ turn.c
@@ -9,13 +9,13 @@
 
 //Function prototypes
 bool moveCheck(int moveType, int curRow, int curCol);
-void merge(square board[BOARD_SIZE][BOARD_SIZE],int k, int t, int curRow, int curCol, int moveNum);
+void merge(square board[BOARD_SIZE][BOARD_SIZE],int k, int t, int curRow, int curCol, int temp);
 void removeExcess(square board[BOARD_SIZE][BOARD_SIZE], int curRow, int curCol, player players[2], int curPlayer);
 bool insert(square board[BOARD_SIZE][BOARD_SIZE], player players[2], int k, int t, int curPlayer);
 
 void playerTurn(player players[2], square board[BOARD_SIZE][BOARD_SIZE], int curPlayer)
 {
-    int k = 0, t = 0, h = 0, moveNum, moveType, curRow, curCol;
+    int k = 0, t = 0, h = 0, moveNum, moveType, curRow, curCol, temp = 0;
 
     printf("It is currently %s's turn\n", players[curPlayer].pname);
 
@@ -68,7 +68,8 @@ void playerTurn(player players[2], square board[BOARD_SIZE][BOARD_SIZE], int cur
     curCol = t;
 
     //Number of squares a piece will move is decide by size of stack
-    for (moveNum = 0; moveNum < board[k][t].num_pieces; moveNum++) {
+    for (moveNum = 0; moveNum < board[k][t].num_pieces; moveNum++)
+    {
         printf("\nYou are currently at position [%d][%d]\n", curRow, curCol);
 
         printf("Please select a direction in which to move the piece\nEnter 1 for left, 2 for right, 3 for up and 4 for down or 5 to end movement at current position:");
@@ -97,10 +98,12 @@ void playerTurn(player players[2], square board[BOARD_SIZE][BOARD_SIZE], int cur
                     break;
                 case 5:
                     moveNum = 5;
+                    temp -= 1;
                     break;
                 default:
                     break;
             }
+            temp += 1;
             continue;
         }
         //Statement will trigger if selected move is not a valid one based on the cursors current position
@@ -111,44 +114,52 @@ void playerTurn(player players[2], square board[BOARD_SIZE][BOARD_SIZE], int cur
         }
     }
 
-    merge(board, k, t, curRow, curCol, moveNum);//Call to merge function which will combine the two identified stacks
-    board[k][t].stack = NULL;//Resets the pointer in the now vacant board square to NULL
-    board[k][t].num_pieces = 0;//Resets the counter for the number of pieces on the now vacant board square to 0
+    merge(board, k, t, curRow, curCol, temp);//Call to merge function which will combine the two identified stacks
 
     //Call to function will trigger if the stack contains more that 5 pieces
     if(board[curRow][curCol].num_pieces>5)
     {
         removeExcess(board, curRow, curCol, players, curPlayer);
     }
+
+
 }
 
 //Function to merge two stacks together
-void merge(square board[BOARD_SIZE][BOARD_SIZE], int k, int t, int curRow, int curCol, int moveNum)
+void merge(square board[BOARD_SIZE][BOARD_SIZE], int k, int t, int curRow, int curCol, int temp)
 {
-    int curPosition = 0;
+    int curPosition = 1;
     piece * curr;//Pointer to a struct of type piece
-    piece * next;
     curr = board[k][t].stack;//Curr is initialised with the address of the top of the stack to be added
-    //While loop will continue execution until the bottom of the stack is reached
-    while(curPosition < moveNum)
+
+    //While loop will continue execution until the curPosition matches temp
+    while(curPosition < temp)
     {
         curr = curr->next;
-        curPosition++;
+        curPosition += 1;
+        printf("Loop triggers\n");
     }
-    
-    if(curr->next == NULL) {
+
+    //Will trigger if entire stack is being moved
+    if(curPosition == board[k][t].num_pieces)
+    {
         curr->next = board[curRow][curCol].stack;//The pointer at the bottom of the stack is updated to point to the top of the other stack
         board[curRow][curCol].stack = board[k][t].stack;//Makes it so that the board pointer now points to the new top of the stack
-        board[curRow][curCol].num_pieces += board[k][t].num_pieces;//Updates the counter for the number of pieces in the stack
+        board[curRow][curCol].num_pieces += temp;//Updates the counter for the number of pieces in the stack
         board[k][t].stack = NULL;//Resets the pointer in the now vacant board square to NULL
         board[k][t].num_pieces = 0;//Resets the counter for the number of pieces on the now vacant board square to 0
-    } else{
-        next = curr->next;
-        curr->next = board[curRow][curCol].stack;
-        board[curRow][curCol].stack = board[k][t].stack;
-        board[curRow][curCol].num_pieces += moveNum;
-        board[k][t].stack = next;
-        board[k][t].num_pieces -= moveNum;
+        return;
+    }
+    //Will trigger if part of stack is being moved
+    else
+    {
+        piece * newTop = curr->next;//The pointer to become the new top of the original stack is held in pointer newTop
+        curr->next = board[curRow][curCol].stack;//Pointer in curr is then reset to point to the top of the other stack
+        board[curRow][curCol].stack = board[k][t].stack;//Makes it so that the board pointer now points to the new top of stack
+        board[curRow][curCol].num_pieces += temp;//Updates the counter for the number of pieces in the stack
+        board[k][t].stack = newTop;//Sets the new top of the original stack as the position previously pointed to by curr->next
+        board[k][t].num_pieces -= temp;//Decreases the number of pieces in the original stack
+        return;
     }
 }
 
